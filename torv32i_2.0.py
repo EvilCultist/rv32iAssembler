9a0ceb255484ab2e6a743b7b840856f1d667e8fa
Chinmaya <chinmaya24163@iiitd.ac.in>
2025-02-14 17:32:05 +0000

    torvi32i_2.0.py

---
+++ torv32i_2.0.py
@@ -0,0 +1,108 @@
+from instructions import r_type, i_type, b_type, s_type, reg_conv
+
+def toBin(x, size=3):
+    if x < 0:
+        return toNegBin(x, size)
+    out = ""
+    while x != 0:
+        out += str(x%2)
+        x//=2
+    out = '0'*(size-len(out[-1::-1])) + out[-1::-1]
+    return out
+
+def toNegBin(x, size):
+    s = toBin(-x-1, size)
+    t = ""
+    for i in s:
+        if i == '1':
+            t += '0'
+        else:
+            t += '1'
+    return t
+
+def getImmediate(x):
+    n = 0
+    while x[n].isdigit():
+        n+=1
+    return toBin(int(x[0:n]),12)
+
+def getRegVal(x):
+    n = 0
+    while not x[n].isalpha():
+        n+=1
+    return toBin(reg_conv[x[n:-1]], 5)
+
+def make_r(ops, registers):
+    registers = registers.split(',')
+    func7 = toBin(ops[2],7)
+    rs2 = toBin(reg_conv[registers[2]],5)
+    rs1 = toBin(reg_conv[registers[1]],5)
+    func3 = toBin(ops[1],3)
+    rd = toBin(reg_conv[registers[0]],5)
+    opcode = toBin(ops[0], 7) 
+    # print(func7, rs2, rs1, func3, rd, opcode, sep='\n')
+    return func7 + rs2 + rs1 + func3 + rd + opcode
+
+def make_i(ops, registers):
+    registers = registers.split(',')
+    if len(registers) == 3:
+        imm = toBin(int(registers[2]), 12)
+        rs1 = toBin(reg_conv[registers[1]], 5)
+    else:
+        imm = getImmediate(registers[1])
+        rs1 = getRegVal(registers[1])
+    func3 = toBin(ops[1],3)
+    rd = toBin(reg_conv[registers[0]],5)
+    opcode = toBin(ops[0], 7) 
+    return  imm + rs1 + func3 + rd + opcode
+
+def make_b(ops, registers, labels, pc):
+    registers = registers.split(',')
+    opcode = toBin(ops[0], 7) 
+    func3 = toBin(ops[1], 3)
+    rs1 = toBin(reg_conv[registers[0]], 5)
+    rs2 = toBin(reg_conv[registers[1]], 5)
+    try:
+        imm = toBin(int(registers[2]), 12)
+    except:
+        val = registers[2]
+        if val not in labels:
+            print(f'label not found - {val}')
+            print(f'labels - {labels}')
+            raise TypeError('Using undefined label')
+        imm = toBin(labels[val]-pc, 12)
+    return imm[11] + imm[9:3:-1] + rs2 + rs1 + func3 + imm[3::-1]+ imm[10] + opcode
+
+def make_s(ops, registers):
+    registers = registers.split(',')
+    opcode = toBin(ops[0], 7)
+    func3 = toBin(ops[1], 3)
+    rs2 = toBin(reg_conv[registers[0]], 5)
+    try:
+        rs1 = toBin(reg_conv[registers[1]], 5)
+    except:
+        imm = getImmediate(registers[1])
+        rs1 = getRegVal(registers[1])
+    return imm[:7] + rs2 + rs1 + func3 + imm[7:] + opcode
+
+def transl(ins, labels, pc):
+    cmd = ins[0]
+    if cmd in r_type:
+        return make_r(r_type[cmd], ins[1])
+    if cmd in i_type:
+        return make_i(i_type[cmd], ins[1])
+    if cmd in b_type:
+        return make_b(b_type[cmd], ins[1], labels, pc)
+    if cmd in s_type:
+        return make_s(s_type[cmd], ins[1])
+    
+if __name__=='__main__':
+    #           tests
+    print(f"binary of 421: {toBin(421)}")
+    print(transl(['add', 'ra,sp,gp'], {}, 0))
+    print(transl(['lw', 't0,32(gp)'], {}, 0))
+    print(transl(['lw', 't0,gp,32'], {}, 0))
+    print(transl(['bne', 'a0,a3,-8'], {}, 0))
+    print(transl(['blt', 'a0,a3,start'], {'start': 0}, 8))
+    print(transl(['sw', 'ra,32(sp)'],{},0))
+
